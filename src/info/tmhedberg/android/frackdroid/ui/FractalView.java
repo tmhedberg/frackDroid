4b7c468df08aaeec4ee80a1a01dd878948387b8c
Taylor Hedberg <tmhedberg@gmail.com>
2010-08-14 16:32:04 -0400

    Skeleton implementation of SurfaceHolder.Callback

--- src/info/tmhedberg/android/frackdroid/ui/FractalView.java
+++ src/info/tmhedberg/android/frackdroid/ui/FractalView.java
@@ -1,13 +1,30 @@
 package info.tmhedberg.android.frackdroid.ui;
 
 import android.content.Context;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class FractalView
-extends SurfaceView {
+extends SurfaceView
+implements SurfaceHolder.Callback {
 	
 	public FractalView(Context context) {
 		super(context);
+	}
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		
+	}
+	
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		
+	}
+	
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		
 	}
 	
 }
