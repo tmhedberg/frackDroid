040281b6830189ebe2dcab8d1390dcdc30a515d0
Taylor Hedberg <tmhedberg@gmail.com>
2010-08-14 12:44:42 -0400

    Define basic activity

---
+++ src/info/tmhedberg/android/frackdroid/FrackDroid.java
@@ -0,0 +1,18 @@
+package info.tmhedberg.android.frackdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrackDroid
+extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+		
+	}
+	
+}
