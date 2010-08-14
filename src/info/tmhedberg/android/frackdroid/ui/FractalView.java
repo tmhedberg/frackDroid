15b7e5520eea11263767d9a226a3221dd8131c79
Taylor Hedberg <tmhedberg@gmail.com>
2010-08-14 14:31:31 -0400

    Constructor

---
+++ src/info/tmhedberg/android/frackdroid/ui/FractalView.java
@@ -0,0 +1,13 @@
+package info.tmhedberg.android.frackdroid.ui;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class FractalView
+extends SurfaceView {
+	
+	public FractalView(Context context) {
+		super(context);
+	}
+	
+}
