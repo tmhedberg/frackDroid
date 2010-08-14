82dcaf8e1c4650af0968e63055c200de038ec6b9
Taylor Hedberg <tmhedberg@gmail.com>
2010-08-14 16:44:38 -0400

    Make method arguments final

--- src/info/tmhedberg/android/frackdroid/ui/FractalView.java
+++ src/info/tmhedberg/android/frackdroid/ui/FractalView.java
@@ -8,22 +8,22 @@ public class FractalView
 extends SurfaceView
 implements SurfaceHolder.Callback {
 	
-	public FractalView(Context context) {
+	public FractalView(final Context context) {
 		super(context);
 	}
 	
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	public void surfaceChanged(final SurfaceHolder holder, final int format, final int width, final int height) {
 		
 	}
 	
 	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
+	public void surfaceCreated(final SurfaceHolder holder) {
 		
 	}
 	
 	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
+	public void surfaceDestroyed(final SurfaceHolder holder) {
 		
 	}
 	
